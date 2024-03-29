1068dce38d7129f789a840cfe456eed8b4e826b2
radeon2525 <e1070572@u.nus.edu>
2024-03-29 20:40:20 +0800

    fix

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -3,23 +3,25 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+
 import seedu.address.model.Model;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
+
 /**
  * Reverts the {@code model}'s address book to its previous state.
  */
 public class UndoCommand extends Command {
-    private static final Logger logger = LogsCenter.getLogger(UndoCommand.class);
 
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_SUCCESS = "Undo success!";
     public static final String MESSAGE_FAILURE = "No more commands to undo!";
+    private final Logger logger = LogsCenter.getLogger(UndoCommand.class);
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
