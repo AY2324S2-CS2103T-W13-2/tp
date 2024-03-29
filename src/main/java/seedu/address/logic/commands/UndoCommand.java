7098d36a83ab8f2fb819faa017c416c5271ca9a8
radeon2525 <e1070572@u.nus.edu>
2024-03-29 20:37:11 +0800

    Add logging

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -3,14 +3,19 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Reverts the {@code model}'s address book to its previous state.
  */
 public class UndoCommand extends Command {
+    private static final Logger logger = LogsCenter.getLogger(UndoCommand.class);
 
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_SUCCESS = "Undo success!";
@@ -20,12 +25,16 @@ public class UndoCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
+        logger.log(Level.INFO, "Attempting to execute UndoCommand");
+
         if (!model.canUndoAddressBook()) {
+            logger.log(Level.WARNING, "No more commands to undo!");
             throw new CommandException(MESSAGE_FAILURE);
         }
 
         model.undoAddressBook();
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        logger.log(Level.INFO, "Undo successful!");
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
